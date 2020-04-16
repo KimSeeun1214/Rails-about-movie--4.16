d6d89abf47195a26a527edfa8b106a16a75f9a76
김세은 <63703349+KimSeeun1214@users.noreply.github.com>
2020-04-16 12:05:18 +0900

    Add files via upload

---
+++ movies_controller.rb
@@ -0,0 +1,54 @@
+class MoviesController < ApplicationController
+	protect_from_forgery with: :null_session
+
+	def new
+		@movie = Movie.new
+	end
+
+	def index
+		@movies= Movie.all
+	end
+	
+	def create
+		@movie = Movie.new(movie_params)
+		#@movie = Movie.new(params[:movie])
+
+		if @movie.save
+			redirect_to @movie
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
+
+	def edit
+		@movie=Movie.find(params[:id])	
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+
+		if @movie.update(movie_params)
+			redirect_to @movie
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+
+		redirect_to movies_path
+	end
+
+	private
+		def movie_params
+			params.require(:movie).permit(:title, :information)
+		end
+
+	
+end
